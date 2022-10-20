09c2b3c129ed818fbe32c3e02228edf5844c90d8
Uhmopt <qethum@gmail.com>
2022-10-21 07:26:26 +0900

    [Remove] loader

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import { homeLoader } from "../service/homeServices";
 import ErrorPage from "../views/pages/errors/ErrorPage";
 import Home from "../views/pages/home";
 import Root from "../views/pages/root";
@@ -9,6 +8,6 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [{ path: "/", element: <Home />, loader: homeLoader }],
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
