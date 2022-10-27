2fe18c55ca965a1e1874584955028a1b35efc8e0
Uhmopt <qethum@gmail.com>
2022-10-27 10:38:00 +0900

    [New] service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,6 +11,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
